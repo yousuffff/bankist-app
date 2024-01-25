0f10ffc1f3beb83d6118ae3e0215094fb0d63fd1
Mohd Yousuf <mdyousuf43201@gmail.com>
2024-01-25 16:42:13 +0530

    practice code

--- script.js
+++ script.js
@@ -166,6 +166,9 @@ btnLogin.addEventListener('click', function (e) {
     //calling function
     updateUI(currentAccount);
   }
+  else {
+    alert('Please enter Valid Credentials');
+  }
 
 })
 btnTransfer.addEventListener('click', function (e) {
@@ -437,5 +440,163 @@ const calcAverageHuman = function (dogs) {
 // calcAverageHuman([16, 6, 10, 5, 6, 1, 4]);
 
 
-let arr = [16, 6, 10, 5, 6, 1, 4];
-console.log(arr.sort((a, b) => a - b));
+// let arr = [16, 6, 10, 5, 6, 1, 4];
+// console.log(arr.sort((a, b) => a - b));
+
+// let arrCopy = arr.slice();
+
+// console.log(arr.slice(2, 6))  //Slice Method
+// console.log(arr)
+
+// console.log(arr.concat(arrCopy))  //concat method
+
+/////This methods mutate the array ///////////
+
+// console.log(arrCopy.push(8)); //push method add element at end of array
+// console.log(arrCopy);
+// console.log(arrCopy.unshift(12)); // unshift method add element at start of array
+// console.log(arrCopy); 
+
+// console.log(arrCopy.pop()); // remove element from end 
+// console.log(arrCopy); 
+// console.log(arrCopy.shift()); // remove element from starting 
+// console.log(arrCopy); 
+// console.log(arrCopy.splice(4, 2)); //splice method
+// console.log(arrCopy);
+// console.log(arrCopy.reverse()); // reverse the array
+// console.log(arrCopy.sort((a, b)=> a-b)); // sort method work differently in js thats why we write function as per need
+
+// const allMovements = accounts.map((acc)=> acc.movements)
+// console.log(allMovements); // to get all movements from all accounts
+// console.log(allMovements.flat()); // unpack all sub array and make one array;
+// // we can use flatmap instead of using map and flat separately
+
+// const allMovements2 = accounts.flatMap((acc)=> acc.movements);
+// console.log(allMovements2); // 
+
+//  const newArr = new Array(7);
+//  newArr.map((i)=> i+1);
+//  console.log(newArr);
+
+// const filteredArr = arr.filter((a)=> a>5); // filter
+// console.log(filteredArr);
+
+// console.log(arr.reduce((acc, curr)=> acc+ curr)); //reduce
+
+// const lenght = 4;
+// console.log(arr); 
+
+// console.log(arr.map(a => a * a));
+
+// console.log(arrCopy.some(a => a <10)); // return true if any element match the condition
+// console.log(arrCopy.every(a => a >10)); // return  true if every element match the condition
+
+// arr.forEach(function(a ,i ) {
+//   console.log(`${i} = ${a}`);
+// });
+
+///////////////////////////////////////
+// Array Methods Practice
+
+// // 1.
+// const bankDepositSum = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov > 0)
+//   .reduce((sum, cur) => sum + cur, 0);
+
+// console.log(bankDepositSum);
+
+// // 2.
+// // const numDeposits1000 = accounts
+// //   .flatMap(acc => acc.movements)
+// //   .filter(mov => mov >= 1000).length;
+
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+// console.log(numDeposits1000);
+
+// // Prefixed ++ oeprator
+// let a = 10;
+// console.log(++a);
+// console.log(a);
+
+// // 3.
+// const { deposits, withdrawals } = accounts
+//   .flatMap(acc => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
+
+// console.log(deposits, withdrawals);
+
+// // 4.
+// // this is a nice title -> This Is a Nice Title
+// const convertTitleCase = function (title) {
+//   const capitzalize = str => str[0].toUpperCase() + str.slice(1);
+
+//   const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+//   const titleCase = title
+//     .toLowerCase()
+//     .split(' ')
+//     .map(word => (exceptions.includes(word) ? word : capitzalize(word)))
+//     .join(' ');
+
+//   return capitzalize(titleCase);
+// };
+
+// console.log(convertTitleCase('this is a nice title'));
+// console.log(convertTitleCase('this is a LONG title but not too long'));
+// console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
+*/
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+console.log(dogs);
+
+dogs.forEach(function(dog){
+  
+})
+
+
